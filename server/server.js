913f3c01002b1b496eb7e678c36923d349a840fc
Alexfranky <alexdjieufo@gmail.com>
2023-03-21 19:02:40 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import { Conversation } from '@maxijonson/gpt-turbo';
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
@@ -17,32 +13,24 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Welcome to Genus SMART AI!'
   })
 });
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
-
+    const conversation = new Conversation({
+      apiKey: process.env.OPENAI_API_KEY,
+  });
+  const response = await conversation.prompt(`${prompt}`);
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response,
     });
 
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send('Network busy. Please try again' || 'Something went wrong');
   }
 });
-
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
+app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
